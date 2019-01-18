fcccd86dc880342d8ccebe4c45baca686080084c
Daniel Burbach <daniel@>
2019-01-18 11:02:01 -0800

    clean up extra script and add to promise methods

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'zipcodes'
+const zipcodes = require('zipcodes');
 import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
