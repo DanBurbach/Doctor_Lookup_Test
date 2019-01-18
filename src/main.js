49dbb3bc96b5ebacf7524c6c8166d4b2597f559c
Daniel Burbach <daniel@>
2019-01-18 13:44:20 -0800

    added API tester for accepting new patients or not, placed into a promise

--- src/main.js
+++ src/main.js
@@ -1,51 +1,55 @@
 import './styles.css';
 import $ from 'jquery';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'zipcodes'
 const zipcodes = require('zipcodes');
+// added an npm for looking up lattitude and longitude by zipcode 'https://www.npmjs.com/package/zipcodes'
 import { Doctor } from './doctor.js';
 
 $(document).ready(function() {
-  $('#medicalIssue-name').click(function() {
-    let issue = $('#medicalIssue-input').val();
-    $('#medicalIssue-input').val("";)
+  const doctor = new Doctor;
+
+  $('#issueSubmit').click(function() {
+    event.preventDefault();
+    const issue = $('#issue').val();
+    const zip = $('#zipcode').val();
+    const zipBreakdown = zipcodes.lookup(zipcode)
+    $('#.doctor_result').empty();
 
 
-
+  function doctorIssueLocation(issue, lattitude, longitude) {
+    let promise = doctor.findDoctor(issue, lattitude, longitude);
     promise.then(function(response) {
       let body = JSON.parse(response);
-    }, function(error) {
+      if (body.data.length == 0) {
+        $("#.doctor_result").text("Sorry, there are no doctors that fit the entered data")
+      }
+      else {
+        body.data.forEach(function(element) {
+          let acceptingNewPatients = element.practices[0].accepts_new_patients
+          function openOrClosed(value) {
+            let answer;
+            if (value == true) {
+              answer = "Yes, they are accepting new patients at this time."
+            }
+            else {answer == "No, we are sorry but they are not accepting any new patients at this time."}
+            return answer
+            }
+
+
+
+          });
+
+        }
+      })
+    };
+
+    },
+
+
+    function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
+    })
 
-
-  $.ajax({
-    url: 'https://developer.betterdoctor.com/?',
-    type: 'GET',
-    data: {
-      format: 'json',
-      paragraphs: '1',
-      words: '1'
-    },
-    success: function(response) {
-      console.log('use API');
-      let issue = new Doctor(response.toLowerCase());
-      let doctor = new Doctor;
-      showIssue(issue);
-      showDoctor(doctor);
-      $('#inquire-area').show();
-    },
-    error: function() {
-      $('#error-message').text('There was a problem proccessing your API.');
-    }
-  });
-
-
-  function showDoctor(name) {
-  }
-
-  function showIssue(issue) {
-
-  }
 });
