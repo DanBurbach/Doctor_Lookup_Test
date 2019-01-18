d8a13875656313e6df1ee7f4e3c2bda22a850a5f
Daniel Burbach <daniel@>
2019-01-18 15:15:05 -0800

    track down syntax error for drIssueLocation function.

--- src/doctor.js
+++ src/doctor.js
@@ -1,31 +1,34 @@
+import { BDApi } from './BDApi.js';
+
 class Doctor  {
-constructor(name) {
+  constructor(name) {
     this.name = name;
   }
-function doctorIssueLocation(issue, lattitude, longitude) {
-  let promise = doctor.findDoctor(issue, lattitude, longitude);
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    if (body.data.length == 0) {
-      $("#.doctor_result").text("Sorry, there are no doctors that fit the entered data")
-    } else {
-      body.data.forEach(function(element) {
-        let acceptingNewPatients = element.practices[0].accepts_new_patients
-        function openOrClosed(value) {
-          let answer;
-          if (value == true) {
-            answer = "Yes, they are accepting new patients at this time."
-          }
-          else {answer == "No, we are sorry but they are not accepting any new patients at this time."}
-          return answer
+
+  function doctorIssueLocation(issue, lattitude, longitude) {
+    let promise = doctor.findDoctor(issue, lattitude, longitude);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      if (body.data.length == 0) {
+        $(".doctor_result").text("Sorry, there are no doctors that fit the entered data")
+      } else {
+        body.data.forEach(function(element) {
+          let acceptingNewPatients = element.practices[0].accepts_new_patients
+          function openOrClosed(value) {
+            let answer;
+            if (value == true) {
+              answer = "Yes, they are accepting new patients at this time."
+            } else {answer == "No, we are sorry but they are not accepting any new patients at this time."}
+            return answer
           }
 
 
         })
-
+      }, function(error) {
+        $('.showErrors').text(`There was an error processing your request: ${error.message}`);
       }
     })
   }
-};
+}
 
 export { Doctor };
