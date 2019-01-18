3deaf53a44f4af6da0947bff16d2a7f37d3a1859
Daniel Burbach <daniel@>
2019-01-18 13:43:25 -0800

    removed erroneous code from detailsDoctor function section of Doctor class

--- src/doctor.js
+++ src/doctor.js
@@ -6,7 +6,7 @@ class Doctor {
   findDoctor(lattitude, longitude, issue) {
     let promise = new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location='+${lattitude}+'%2C'+${longitude}+'%2C100&user_key='+${process.env.exports.apiKey};
+      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=${lattitude}%2C${longitude}%2C100&user_key=${process.env.exports.apiKey}';
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -18,21 +18,15 @@ class Doctor {
       request.send();
     });
 
-    $.get(url, function(doctor-search) {
-      let template = Handlebars.compile(document.getElementById('doctor-search').innerHTML);
-      document.getElementById('doctor-result').innerHTML = template(doctor-search);
-    });
-
   }
 
   detailsDoctor() {
-    let api_key = process.env.exports.apiKey;
     let doctor_uid = '';
-    let url = 'https://api.betterdoctor.com/2016-03-01/doctors/' + doctor_uid + '?user_key=' + ${process.env.exports.apiKey};
+    let url = 'https://api.betterdoctor.com/2016-03-01/doctors/' + doctor_uid + '?user_key=' + '${process.env.exports.apiKey}';
 
-    $.get(url, function (doctor-search) {
+    $.get(url, function (doctor_search) {
       let template = Handlebars.compile(document.getElementById('doc-template').innerHTML);
-      document.getElementById('doctor-result').innerHTML = template(doctor-search);
+      document.getElementById('doctor_result').innerHTML = template(doctor_search);
     });
   }
 
