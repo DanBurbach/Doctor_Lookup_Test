4907efb56ca9127eae7b6288692d0fc75c2b6945
Daniel Burbach <daniel@>
2019-01-18 14:52:54 -0800

    add BDApi for API clean up

---
+++ src/BDApi.js
@@ -0,0 +1,36 @@
+class BDApi {
+
+  findDoctor(lattitude, longitude, issue) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=${lattitude}%2C${longitude}%2C100&skip=0&limit=20&user_key=${process.env.exports.apiKey}';
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  findDoctorByName(firstName,lastName) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&skip=0&limit=20&user_key=${process.env.exports.apiKey}';
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+}
+export { BDApi };
