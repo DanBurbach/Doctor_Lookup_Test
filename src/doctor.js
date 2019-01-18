1c57b5a02aa4e4156ea142a47408b0eb1e6c0709
Daniel Burbach <daniel@>
2019-01-18 09:42:37 -0800

    add promise for findDoctor function

--- src/doctor.js
+++ src/doctor.js
@@ -6,7 +6,27 @@
 
   }
 
+  findDoctor(zipcode, issue) {
+    let promise = new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=' + exports.apiKey;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+    
+    $.get(url, function(doctor-search) {
+      let template = Handlebars.compile(document.getElementById('doctor-search').innerHTML);
+      document.getElementById('doctor-result').innerHTML = template(doctor-search);
+    });
 
+  }
 
 
 
