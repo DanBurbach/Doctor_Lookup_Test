95bc0d6ce86cb7509314c811e9a66956aa5d8c8a
Daniel Burbach <dburbach1982@gmail.com>
2019-01-20 11:01:57 -0800

    remove extra search function (not needed)

--- src/doctor.js
+++ src/doctor.js
@@ -23,22 +23,6 @@ class Doctor  {
     });
   }
 
-  findDoctorByName(firstName,lastName) {
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&skip=0&limit=35&user_key=${apiKey}';
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-
   function doctorIssueLocation(name, query, user_location) {
     let promise = doctor.findDoctor(name, query, user_location);
     promise.then(function(response) {
