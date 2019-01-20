ee07d4ac0dbe6cec1b4616661fe1788ba97c51b8
Daniel Burbach <dburbach1982@gmail.com>
2019-01-20 10:56:53 -0800

    remove doctor search related functions and place in doctor.js, work on adding all Better Doctor API functions here instead

--- src/BDApi.js
+++ src/BDApi.js
@@ -1,38 +1,11 @@
 import { Doctor } from './doctor.js';
 
 class BDApi {
+  constructor() {
 
-  findDoctor(lattitude, longitude, issue) {
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/practices?location=${lattitude}%2C${longitude}%2C100&skip=0&limit=20&user_key=${process.env.exports.apiKey}';
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
   }
 
-  findDoctorByName(firstName,lastName) {
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = 'https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&skip=0&limit=20&user_key=${process.env.exports.apiKey}';
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+
 
 }
 export { BDApi };
