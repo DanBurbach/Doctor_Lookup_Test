4363fb507aeee108757c37387489604dc266b123
Daniel Burbach <dburbach1982@gmail.com>
2019-01-20 17:32:03 -0800

    code tidy up and move

--- src/main.js
+++ src/main.js
@@ -3,19 +3,18 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Doctor } from './doctor.js';
-import { BDApi } from './BDApi.js';
+import { PositiveResult, NegativeResult, ErrorResult } from './BDApi.js';
 
 $(document).ready(function() {
   const doctors = new Doctor
-  const api = new BDApi
 
   $('#issueSubmit').click(function(event) {
     event.preventDefault();
       const query = $('#query').val();
-      const user_location_slug = ((this.user_location_state + "-" + this.user_location_city).toLowerCase().val());
+      const location_slug = ((this.user_location_state + "-" + this.user_location_city).toLowerCase().val());
       const name = $('#name').val();
       $('#doctor_result').empty();
-      const promise = doctor.findDoctor(name, query, user_location_slug);
-      PositiveResult(name, query, user_location_slug, promise);
+      const promise = doctors.findDoctor(name, query, location_slug);
+      PositiveResult(name, query, location_slug, promise);
     })
 });
