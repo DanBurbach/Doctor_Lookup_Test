6789fc95c5b12de35e84c4e4cd1c12b01a4fee6f
Daniel Burbach <dburbach1982@gmail.com>
2019-01-20 23:41:10 -0800

    grey out let cors variable. causes issue working on Windows computer

--- src/doctor.js
+++ src/doctor.js
@@ -1,11 +1,9 @@
 class Doctor  {
-
   findDoctor(name, query, location_slug) {
     return new Promise(function(resolve, reject) {
-      let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : '';
-      const url = '${cors}https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location_slug=${location_slug}&user_location_slug=or-portland&skip=0&limit=30&accepts_new_patients=true&user_key=${apiKey}';
-      const request = new XMLHttpRequest();
-
+    let request = new XMLHttpRequest();
+    // let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : ''; (add ${cors} to front of url if issue)
+    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location_slug=${location_slug}&user_location_slug=or-portland&skip=0&limit=30&accepts_new_patients=true&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -13,7 +11,6 @@ class Doctor  {
           reject(Error(request.statusText));
         }
       };
-
       request.open("GET", url, true);
       request.send();
     });
