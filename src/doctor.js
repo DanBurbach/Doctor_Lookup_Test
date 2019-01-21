3caf2fe60822ffaaacb544e2270b4983da60fcbb
Daniel Burbach <dburbach1982@gmail.com>
2019-01-20 17:37:26 -0800

    change const url in findDoctor method to return proper data

--- src/doctor.js
+++ src/doctor.js
@@ -1,18 +1,10 @@
-import { apiKey } from '../.env';
-import { BDApi } from './BDApi.js';
-
 class Doctor  {
-  constructor(query) {
-    this.name = name;
-    this.query = query;
-    this.user_location_slug = user_location;
-  }
 
-  findDoctor(name, query, user_location) {
-    let promise = new Promise(function(resolve, reject) {
+  findDoctor(name, query, location_slug) {
+    return new Promise(function(resolve, reject) {
       let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : '';
+      const url = '${cors}https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location_slug=${location_slug}&user_location_slug=or-portland&skip=0&limit=30&accepts_new_patients=true&user_key=${apiKey}';
       const request = new XMLHttpRequest();
-      const url = '${cors}https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}0&location_slug=or-portland&user_location_slug=${user_location_slug}&distance=15&skip=0&limit=35&accepts_new_patients=true&user_key=${apiKey}';
 
       request.onload = function() {
         if (this.status === 200) {
@@ -20,7 +12,8 @@ class Doctor  {
         } else {
           reject(Error(request.statusText));
         }
-      }
+      };
+
       request.open("GET", url, true);
       request.send();
     });
