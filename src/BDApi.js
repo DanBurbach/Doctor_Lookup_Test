9fcc81cd553b31b0f81491f1245a2d2a0cbb2e9b
Daniel Burbach <dburbach1982@gmail.com>
2019-01-21 23:53:58 -0800

    fix code to return query. still need to program search by name

--- src/BDApi.js
+++ src/BDApi.js
@@ -1,51 +1,41 @@
 import $ from 'jquery';
 
-export function PositiveResult(name, query, location_slug, promises) {
-  let promise = promises;
-  promise.then(function(response) {
+function positiveResult(query, location_slug, promises) {
+  let promiseDoctors = promises;
+  console.log(promiseDoctors);
+  promiseDoctors.then(function(response) {
     let doctors = JSON.parse(response);
+    console.log(doctors);
     if (doctors.data.length == 0) {
-      $(".doctor_result").text("Sorry, there are no doctors that fit the entered data")
+      $(".doctor_result").append("Sorry, there are no doctors that fit the entered data");
   } else {
     doctors.data.forEach(function(doctor) {
-    let value = doctor.practices[0].accepts_new_patients
-    function yesOrno(response) {
-    let answer;
-      if (response == true) {
-        answer = "Yes, they are accepting new patients at this time."
-    } else {answer == "No, we are sorry but they are not accepting any new patients at this time."}
-      return answer
-    }
-      const acceptingNewPatients = yesOrno(value);
-      $(".doctor_result").append(`
-            <div class = "card">
-                <h2>Name:</h2>
-                  <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3>
-                <h3>Address:</h3>
-                  <p>${doctor.practices[0].name}</p>
-                  <p>${doctor.practices[0].visit_address.street}</p>
-                  <p>${doctor.practices[0].visit_address.state_long}</p>
-                  <p>${doctor.practices[0].visit_address.zip}</p>
-                <h6>Accepting New Patients</h6>
-                  <p>${acceptingNewPatients}</p>
-                  <h3>Biography</h3>
-                  ${doctor.profile.dynamic_bio}
-                    <table>
-                      <tr>
-                        <th>Picture</th>
-                        <td><img src="${doctor.profile.image_url}"></img></td>
-                      </tr>
+      function yesOrno(response) {
+        let answer;
+        if (response == true) {
+          answer = "Yes, they are accepting new patients at this time."
+        } else {answer == "No, we are sorry but they are not accepting any new patients at this time."}
+          return answer
+      } const acceptingNewPatients = yesOrno(doctor.practices[0].accepts_new_patients);
 
-                      <tr>
-                        <th>Specialties</th>
-                        <td>
-                        ${doctor.profile.specialties}
-                        </td>
-                      </tr>
-                    </table>
-                </div>`
-              ).show();
-          })
+      $(".doctor_result").append(`
+          <div class = "card">
+            <div class = "card-body">
+          <td><img src="${doctor.profile.image_url}"></img></td>
+            <h3>${doctor.profile.first_name} ${doctor.profile.last_name}, ${doctor.profile.title}</h3>
+            <h3>Address:</h3>
+              <p>${doctor.practices[0].name}</p>
+              <p>${doctor.practices[0].visit_address.street}</p>
+              <p>${doctor.practices[0].visit_address.city}, ${doctor.practices[0].visit_address.state_long} ${doctor.practices[0].visit_address.zip}</p>
+            <h5>Website</h5>
+              <p>${doctor.practices[0].website}</p>
+            <h5>Accepting New Patients</h5>
+              <p>${acceptingNewPatients}</p>
+            <h5>Biography</h5>
+              ${doctor.profile.bio}
+              </div>
+            </div><br>`);
+          });
         }
       },
     function(error) {
@@ -53,6 +43,8 @@ export function PositiveResult(name, query, location_slug, promises) {
         <div class = "errorBody">
         <p>An error of <code>${error}</code></p>
         <p>Please attempt your search again, thank you for your patience.</p>
-        </div>`).show();
-      });
+        </div>`)},
+    )
 }
+
+export { positiveResult };
--- src/doctor.js
+++ src/doctor.js
@@ -2,8 +2,8 @@ class Doctor  {
   findDoctor(name, query, location_slug) {
     return new Promise(function(resolve, reject) {
     let request = new XMLHttpRequest();
-    // let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : ''; (add ${cors} to front of url if issue)
-    const url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location_slug=${location_slug}&user_location_slug=or-portland&skip=0&limit=30&user_key=${process.env.exports.apiKey}`;
+    // let cors = (true) ? 'https://cors-anywhere.herokuapp.com/' : ''; (add ${cors} to front of url if cors issue)
+    let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&query=${query}&location_slug=${location_slug},100&user_location_slug=or-portland&skip=0&limit=25&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
